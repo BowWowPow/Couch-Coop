4a30bfaf511001ed0ac9a5100dfd402a80189eb7
Sean Farrell <sfarrell@backflipstudios.com>
2017-09-17 00:23:14 -0600

    Added Ammo. Added a few materials. Added a shooting script. Code - Trying to add an ammo class that controls the ammos colour changes.

---
+++ Couch-Coop/Assets/_Scripts/ammoLife.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoLife : MonoBehaviour {
+
+	public float lifeTime, t;
+	// Use this for initialization
+	void Start () {
+		lifeTime = 10.0f;
+		t = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (t >= lifeTime) 
+		{
+			Destroy ();
+		} 
+			else 
+		{
+			t += Time.deltaTime;
+		}
+	}
+
+	private void resetT (){
+		t = 0.0f;
+	}
+
+	public void Destroy(){
+		Transform.Destroy ();
+
+	}
+}
